b9b08bb9410a4b756876af10fc4639c234e890f2
Phumphathai Chansriwong <ouoam2555@gmail.com>
2020-01-09 13:14:26 +0700

    get data from jan08 and jan09

--- component/mqtt.js
+++ component/mqtt.js
@@ -19,8 +19,12 @@ class mqttEmitter extends EventEmitter {
       console.log("Connect to MQTT successful.");
       mqttEmitterThis.MQTTclient.subscribe("tgr2020/track/data/#");
       mqttEmitterThis.MQTTclient.subscribe("tgr2020/pm25/data/#");
+      mqttEmitterThis.MQTTclient.subscribe("tgr2020/jan08/data/#");
+      mqttEmitterThis.MQTTclient.subscribe("tgr2020/jan09/data/#");
       mqttEmitterThis.MQTTclient.subscribe("/tgr2020/track/data/#");
       mqttEmitterThis.MQTTclient.subscribe("/tgr2020/pm25/data/#");
+      mqttEmitterThis.MQTTclient.subscribe("/tgr2020/jan08/data/#");
+      mqttEmitterThis.MQTTclient.subscribe("/tgr2020/jan09/data/#");
     });
 
     this.MQTTclient.on("message", function(topic, message) {
@@ -30,6 +34,9 @@ class mqttEmitter extends EventEmitter {
       }
       if (topics[0] == "tgr2020" && topics[2] == "data") {
         let msg = message.toString();
+        if (topics[1] == "jan08" || topics[1] == "jan09") {
+          topics[1] = "pm25";
+        }
         try {
           let json = JSON.parse(msg);
           let obj = { ts: new Date(), sensor_type: topics[1], sensor_id: Number(topics[3]), data: json };
